b6168ccf24165c491c77eba6ec5431fa4e935755
Elson Liu <lxd_elson@tju.edu.cn>
2016-04-15 14:38:24 +0800

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -9,5 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello Lizetian!" );
+        System.out.println( "This is for LiuXindi Edit" );
     }
 }
